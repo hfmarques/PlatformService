5c1aa2298350cda2367a668d1b31222947c2a1c9
Héber Marques <heber.f.marques@gmail.com>
2022-10-14 17:56:12 -0300

    Added model and dbcontext

--- PlatformService/Models/Platform.cs
+++ PlatformService/Models/Platform.cs
@@ -2,5 +2,8 @@
 
 public class Platform
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Publisher { get; set; }
+    public string Cost { get; set; }
 }
