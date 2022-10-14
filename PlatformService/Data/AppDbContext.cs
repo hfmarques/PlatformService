bb3782046253c81dca2c1c2c9a9799df417458cd
Héber Marques <heber.f.marques@gmail.com>
2022-10-14 20:07:14 -0300

    Added id on seeding data

--- PlatformService/Data/AppDbContext.cs
+++ PlatformService/Data/AppDbContext.cs
@@ -19,15 +19,15 @@ public class AppDbContext : DbContext
         {
             new()
             {
-                Name = "Dot Net", Publisher = "Microsoft", Cost = "Free"
+                Id = 1, Name = "Dot Net", Publisher = "Microsoft", Cost = "Free"
             },
             new()
             {
-                Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free"
+                Id = 2, Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free"
             },
             new()
             {
-                Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free"
+                Id = 3, Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free"
             }
         };
 
